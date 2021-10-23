7f8d915fca08cbc04bb2d2f10415582b9aea2867
ayushjain27 <ayushjain201574@gmail.com>
2021-10-23 09:44:25 +0530

    add new folder

--- messenger.js
+++ messenger.js
@@ -1,5 +1,24 @@
 let ayushkumar = document.getElementById('ayushkumar');
+let ayushdon = document.getElementById('ayushdon');
+let ayush = document.getElementById('ayush');
+let ayushking = document.getElementById('ayushking');
+
 ayushkumar.addEventListener('click', function () {
     document.location.href = 'ayushkumar.html';
+    // console.log('ayush');
+})
+
+ayushdon.addEventListener('click', function () {
+    document.location.href = 'ayushdon.html';
+    // console.log('ayush');
+})
+
+ayush.addEventListener('click', function () {
+    document.location.href = 'ayush.html';
+    // console.log('ayush');
+})
+
+ayushking.addEventListener('click', function () {
+    document.location.href = 'ayushking.html';
     // console.log('ayush');
 })
